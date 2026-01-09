e73ce594fe3d823f0918612f68df0d3473a42fbb
Yuval nadam <Yuval.nadam@post.runi.ac.il>
2026-01-09 10:37:38 +0100

    fatching prices from coingecko to the dashboard

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'; // manage the data
 import { useNavigate } from 'react-router-dom'; // moving between pages
 import './Dashboard.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 function Dashboard() {
+
+  const [bitcoin, setBitcoin] = useState("");
+  const [ethereum, setEthereum] = useState("");
+  
+  useEffect(() => {
+    const fetchPrices = async () => {
+        try {
+            const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,Tether&vs_currencies=usd');
+            
+            setBitcoin(response.data.bitcoin.usd);
+            setEthereum(response.data.ethereum.usd);
+        } catch (error) {
+            console.error("Error while fatching the prices", error);
+        }
+    };
+
+    fetchPrices(); }, []); //only once
+
     return (
       
       <div className="dashboard-container">
         <section className="dashboard-box-prices">
           <h2>Prices</h2>
+          <p>Bitcoin Price: ${bitcoin}</p>
+          <p>ethereum Price: ${ethereum}</p>
         </section>
   
         <section className="dashboard-box-news">
